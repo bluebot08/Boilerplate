c0a257090bd837de59e392cbd2084f66a76d3593
Stefan O'Neil <oneil.stefan@gmail.com>
2023-04-11 23:45:08 +0900

    Improved boilerplate code

--- express.js
+++ express.js
@@ -1,12 +1,22 @@
+// Install Modules
+
 const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
 
 // initialze express
 
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send("Server is up and running");
-});
+// Set express to serve static pages and images
+
+app.use("/public", express.static("public"));
+
+// initialze body-parser
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Set express to listen to port 3000 with message
 
 app.listen(3000, function () {
   console.log("Server is running on port 3000.");
